99c0b4e2f6e78876f963f5224b65351ab43f74c7
kmanbxv <kmanbxv@gmail.com>
2017-01-19 14:28:16 -0500

    redo graphics

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function pond(){
-  document.getElementById("player").src="pond.m4a";
+  document.getElementById("player").src="pond.m4a"; //play appropriate mp3
+  document.getElementById("playing").innerHTML="Current relaxation soundtrack: <i>pond</i>.";
 }
