c23451c346c9089329bc4a5dda2531910844219d
kmanbxv <kmanbxv@gmail.com>
2017-02-09 16:47:10 -0500

    added second page
    random meditation button, display name of the file playing

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-function pond(){
-  document.getElementById("player").src="pond.m4a"; //play appropriate mp3
-  document.getElementById("playing").innerHTML="Current relaxation soundtrack: <i>pond</i>.";
+// put mp3s in array
+var meditations = ["pond","snow","clouds"];
+
+function play(file){
+    document.getElementById("player").src = meditations[file]+".m4a"; // play appropriate mp3
+    document.getElementById("playing").innerHTML = "Current relaxation soundtrack: <i>"+meditations[file]+"</i>.";
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// pull up a random mp3
+function playRandomFile(){
+    var player = document.getElementById("player");
+
+    // select random mp3
+    var random = getRandomIntInclusive(0, meditations.length-1);
+
+    // change src of player to chosen mp3, make player visible
+    //document.getElementById("player-holder").style="display: block;";
+
+
+
+    //document.write(meditations[];); // debug
+    player.src=meditations[random]+".m4a";
+    document.getElementById("playing").innerHTML = "Current relaxation soundtrack: <i>"+meditations[random]+"</i>.";
+
 }
